0561008e0a8cc4f823a0f5299c301304cdcf3048
iismaelar <iismaelar@gmail.com>
2025-09-23 14:19:22 +0200

    onClick en Guitar
    Añadida funcion de click y agregar al carrito

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { db } from './data/db'
 function App() {  
 
     const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
 
     
   return (
@@ -19,8 +20,10 @@ function App() {
         <div className="row mt-5">
             {data.map((guitar) => (
                 <Guitar 
-                key= {guitar.id}
+                    key= {guitar.id}
                     guitar={guitar}
+                    cart={cart}
+                    setCart={setCart}
                 />
                 ))}
             
